d230b62faa22c70aece2b6e3cfb9805dc9b28fdc
James Ranson <james@agrology.ag>
2023-09-09 16:26:12 -0600

    fix linter warning

--- src/components/graphics/wordmark/Wordmark.tsx
+++ src/components/graphics/wordmark/Wordmark.tsx
@@ -3,7 +3,7 @@ import './Wordmark.css'
 export function Wordmark() {
   return (
     <svg className="wordmark">
-      <text x="0" y="20" fill="#ffffff" font-size="18px" font-family="Verdana" >Example Wordmark</text>
+      <text x="0" y="20">Example Wordmark</text>
     </svg>
   )
 }
